27925baeb8bf81640d54657cf586604e74fd074a
jsdnlb <danielb.alzate1@gmail.com>
2020-04-29 19:15:21 -0500

    Modificación información matriz

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,8 @@
         </h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
+          Aquí está todo lo que necesitas wey! <br>
+          https://code.likeagirl.io/create-a-nxn-matrix-spiral-with-javascript-955ee18402f7
           <br>please join our online
           <a
             href="https://community.vuetifyjs.com"
